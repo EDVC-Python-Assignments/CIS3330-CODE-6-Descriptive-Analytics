5ef784c07a5c0bdd5c8a4b3cc784c681daeec4cd
davicompu <villacis@vt.edu>
2023-03-04 00:57:55 -0700

    Updating template

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CIS3330-CODE-6-Descriptive-Analytics
+## Instructions
---
+++ code_6.py
@@ -0,0 +1,12 @@
+import pandas as pd
+filename = "big-mac-full-index.csv"
+
+def get_describe_country(country_code):
+    pass
+
+def get_country_groupby_country(country_code):
+    pass
+
+
+if __name__ == "__main__":
+    pass
